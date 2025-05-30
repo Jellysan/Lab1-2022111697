9031b00786c7263232aeeb24ce10f2a1d81774bf
Jellysan <smallpingtou@163.com>
2025-05-30 11:00:15 +0800

    Lab3w

---
+++ cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Code/Clion/software_engineering/lab1_git/lib/googletest/googletest
+# Build directory: D:/Code/Clion/software_engineering/lab1_git/cmake-build-debug/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
